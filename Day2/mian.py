ef59686d1e6e81c93c339c26f33b64307676ba4f
sz6084 <sz6084@users.noreply.github.com>
2024-12-01 22:39:44 -0800

    solution works

--- Day2/mian.py
+++ Day2/mian.py
@@ -1,18 +1,43 @@
+## PART ONE ##
 import pathlib
 
-lines = pathlib.Path("exinput.txt").read_text().split("\n")
-
+lines = pathlib.Path("input.txt").read_text().split("\n")
+reports = []
 numSafe = 0
+
 for line in lines:
-    for i in range(3,len(line)):
-        if(line[1]>line[0]):
-            if(line[i+1]<line[i]):
+    reports.append(list(map(int, line.split())))
+
+#def isSafe(report)->bool:
+def isSafe(report):
+    diffs=[]
+    for i in range(1,len(report)):
+        diffs.append(report[i]-report[i-1])
+    for num in diffs:
+        if diffs[0]>0 and num<0:
+            return False
+        elif diffs[0]<0 and num>0:
+            return False
+        if abs(num)>3 or num==0:
+            return False
+    return True
+            
+for report in reports:
+    if isSafe(report):
+        numSafe+=1
+#print(numSafe)
+
+## PART TWO ##
+numSafe2 = 0
+for report in reports:
+    if isSafe(report):
+        numSafe2+=1
+    else:
+        reportCopy = report[:]
+        for i in range(len(reportCopy)):
+            del reportCopy[i]
+            if isSafe(reportCopy):
+                numSafe2+=1
                 break
-            print(line[i+1]+line[i])
-            numSafe+=1
-        else:
-            if(line[i+1]>line[i]):
-                break
-            print(line[i+1]+line[i])
-            numSafe+=1
-print(numSafe)
+            reportCopy = report[:]
+print(numSafe2)
